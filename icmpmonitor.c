c9b16ece585295b83d84cc148d30071d49b87b2e
Aaron Taylor <ataylor@subgeniuskitty.com>
2019-09-21 02:34:05 -0700

    Reordered functions to eliminate declarations. Combined up,down in host struct into hostup bool. Eliminated trailing whitespace.

--- icmpmonitor.c
+++ icmpmonitor.c
@@ -1,6 +1,6 @@
 /*
  * Monitor hosts using ICMP "echo" and notify when down.
- * 
+ *
  * © 2019 Aaron Taylor <ataylor at subgeniuskitty dot com>
  * © 1999 Vadim Zaliva <lord@crocodile.org>
  * © 1989 The Regents of the University of California & Mike Muuss
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <signal.h>
 #include <string.h>
 #include <sys/time.h>
@@ -40,27 +41,14 @@ struct monitor_host {
     int                socket;
     struct timeval     last_ping_received;
     struct timeval     last_ping_sent;
-    /* TODO: Replace the following two values with a single boolean */
-    int                up;
-    int                down;
+    bool               hostup;
     struct sockaddr_in dest;
     unsigned int       sentpackets;
     unsigned int       recvdpackets;
-    
+
     /* linked list */
     struct monitor_host * next;
 };
-
-/* protos */
-static int  gethostaddr(const char * name);
-static void read_hosts(const char * cfg_file_name);
-static void init_hosts(void);
-static void get_response(void);
-static void pinger(int);
-static int  in_cksum(unsigned short * addr, int len);
-static void read_icmp_data(struct monitor_host * p);
-static void tvsub(struct timeval * out, struct timeval * in);
-static int  gcd(int x, int y);
 
 /* globals */
 static struct monitor_host ** hosts       = NULL;
@@ -68,50 +56,6 @@ static int                    isVerbose   = 0;
 static int                    keepBanging = 0;
 static unsigned short         ident;
 static int                    send_delay  = 1;
-
-int
-main(int argc, char ** argv)
-{
-    extern char * optarg;
-    extern int optind;
-    char * cfgfile = NULL;
-    int param;
-
-    while ((param = getopt(argc, argv, "rvf:")) != -1) {
-        switch(param) { 
-            case 'v':
-                isVerbose = 1;
-                break; 
-            case 'r':
-                keepBanging = 1;
-                break; 
-            case 'f':  
-                cfgfile=strdup(optarg);  
-                break;  
-            default: 
-                fprintf(stderr,"Usage: icmpmonitor [-v] [-r] [-f cfgfile]\n");
-                exit(EXIT_FAILURE);
-        } 
-    }
-    
-    if (!cfgfile) {
-        fprintf(stderr, "ERROR: No config file specified.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    read_hosts(cfgfile);
-    
-    init_hosts();
-    
-    ident = getpid() & 0xFFFF;
-    
-    signal(SIGALRM, pinger);
-    alarm(send_delay); 
-
-    get_response();
-
-    exit(EXIT_SUCCESS);
-}
 
 /*
  * Checksum routine for Internet Protocol family headers
@@ -123,7 +67,7 @@ in_cksum(unsigned short * addr, int len)
     unsigned short * w = addr;
     int sum = 0;
     unsigned short answer = 0;
-    
+
     /*
      * Our algorithm is simple, using a 32 bit accumulator (sum), we add
      * sequential 16 bit words to it, and at the end, fold back all the
@@ -133,18 +77,33 @@ in_cksum(unsigned short * addr, int len)
         sum += *w++;
         nleft -= 2;
     }
-    
+
     /* mop up an odd byte, if necessary */
     if (nleft == 1) {
         *(u_char *)(&answer) = *(u_char *)w;
         sum += answer;
     }
-    
+
     /* add back carry outs from top 16 bits to low 16 bits */
     sum = (sum >> 16) + (sum & 0xffff); /* add hi 16 to low 16 */
     sum += (sum >> 16);                 /* add carry */
     answer = ~sum;                      /* truncate to 16 bits */
     return(answer);
+}
+
+/*
+ * Subtracts two timeval structs.
+ * Ensure out >= in.
+ * Modifies out = out - in.
+ */
+static void
+tvsub(register struct timeval * out, register struct timeval * in)
+{
+    if ((out->tv_usec -= in->tv_usec) < 0) {
+        --out->tv_sec;
+        out->tv_usec += 1000000;
+    }
+    out->tv_sec -= in->tv_sec;
 }
 
 /*
@@ -166,16 +125,14 @@ pinger(int ignore)
     while (p) {
         if (p->socket != -1) {
             struct timeval now;
-            
+
             gettimeofday(&now, (struct timezone *) NULL);
             tvsub(&now, &p->last_ping_received);
 
-            if (now.tv_sec > (p->max_delay + p->ping_interval)) {            
-                p->up = 0;
-                if ((!p->down) || keepBanging) {
-                    p->down = 1;
-                    
-                    if (isVerbose) printf("INFO: Host %s is down. Executing DOWN command.\n", p->name);
+            if (now.tv_sec > (p->max_delay + p->ping_interval)) {
+                if ((p->hostup) || keepBanging) {
+                    if (isVerbose) printf("INFO: Host %s stopped responding. Executing DOWN command.\n", p->name);
+                    p->hostup = false;
                     if (!fork()) {
                         system(p->downcmd);
                         exit(EXIT_SUCCESS);
@@ -184,10 +141,10 @@ pinger(int ignore)
                     }
                 }
             }
-            
+
             gettimeofday(&now, (struct timezone *) NULL);
             tvsub(&now, &p->last_ping_sent);
-            
+
             if (now.tv_sec > p->ping_interval) { /* Time to send ping */
                 icp = (struct icmp *) outpack;
                 icp->icmp_type  = ICMP_ECHO;
@@ -197,18 +154,18 @@ pinger(int ignore)
                 icp->icmp_id    = ident;
 
                 if (isVerbose) printf("INFO: Sending ICMP packet to %s.\n", p->name);
-                
+
                 gettimeofday((struct timeval *) &outpack[8], (struct timezone *) NULL);
-                
+
                 cc = DEFAULTDATALEN + 8;  /* skips ICMP portion */
-                
+
                 /* compute ICMP checksum */
                 icp->icmp_cksum = in_cksum((unsigned short *) icp, cc);
-                
+
                 i = sendto(p->socket, (char *) outpack, cc, 0, (const struct sockaddr *) (&p->dest), sizeof(struct sockaddr));
-                
+
                 gettimeofday(&p->last_ping_sent, (struct timezone *) NULL);
-                
+
                 if (i < 0 || i != cc) {
                     if (i<0) fprintf(stderr, "WARN: Failed sending ICMP packet to %s.\n", p->name);
                 }
@@ -217,9 +174,62 @@ pinger(int ignore)
         }
         p = p->next;
     }
-    
+
     signal(SIGALRM, pinger); /* restore handler */
     alarm(send_delay);
+}
+
+static void
+read_icmp_data(struct monitor_host * p)
+{
+    int cc, iphdrlen, delay;
+    socklen_t fromlen;
+    struct sockaddr_in from;
+    struct ip * ip;
+    struct icmp * icmp;
+    struct timeval tv;
+    unsigned char buf[MAXPACKETSIZE];
+
+    gettimeofday(&tv, (struct timezone *) NULL);
+
+    fromlen = sizeof(from);
+    if ((cc = recvfrom(p->socket, buf, sizeof(buf), 0, (struct sockaddr *)&from, &fromlen)) < 0) {
+        if (errno != EINTR) fprintf(stderr, "WARN: Error reading ICMP data from %s.\n", p->name);
+        return;
+    }
+
+    /* check IP header actual len */
+    ip       = (struct ip *) buf;
+    iphdrlen = ip->ip_hl << 2;
+    icmp     = (struct icmp *) (buf + iphdrlen);
+
+    if (cc < iphdrlen + ICMP_MINLEN) {
+        fprintf(stderr, "WARN: Received short packet from %s.\n", p->name);
+        return;
+    }
+
+    if (icmp->icmp_type == ICMP_ECHOREPLY && icmp->icmp_id == ident && icmp->icmp_seq == p->socket) {
+        p->recvdpackets++;
+
+        memcpy(&p->last_ping_received, &tv, sizeof(tv));
+
+        tvsub(&tv, (struct timeval *) &icmp->icmp_data[0]);
+        delay = tv.tv_sec * 1000 + (tv.tv_usec / 1000);
+
+        if (isVerbose) printf("INFO: Got ICMP reply from %s in %d ms.\n", p->name, delay);
+        if (!p->hostup) {
+            if (isVerbose) printf("INFO: Host %s started responding. Executing UP command.\n", p->name);
+            p->hostup = true;
+            if (!fork()) {
+                system(p->upcmd);
+                exit(EXIT_SUCCESS);
+            } else {
+                wait(NULL);
+            }
+        }
+    } else {
+        /* TODO: Do anything here? */
+    }
 }
 
 static void
@@ -228,7 +238,7 @@ get_response(void)
     fd_set rfds;
     int retval, maxd = -1;
     struct monitor_host * p;
-    
+
     while (1) {
         p = hosts[0];
         FD_ZERO(&rfds);
@@ -237,7 +247,7 @@ get_response(void)
                 if (p->socket > maxd) maxd=p->socket;
                 FD_SET(p->socket, &rfds);
             }
-            p=p->next;
+            p = p->next;
         }
 
         retval = select(maxd+1, &rfds, NULL, NULL, NULL);
@@ -258,70 +268,16 @@ get_response(void)
 }
 
 static void
-read_icmp_data(struct monitor_host * p)
-{
-    int cc, iphdrlen, delay;
-    socklen_t fromlen;
-    struct sockaddr_in from;
-    struct ip * ip;
-    struct icmp * icmp;
-    struct timeval tv       ;
-    unsigned char buf[MAXPACKETSIZE];
-
-    gettimeofday(&tv, (struct timezone *) NULL);
-    
-    fromlen = sizeof(from);
-    if ((cc = recvfrom(p->socket, buf, sizeof(buf), 0, (struct sockaddr *)&from, &fromlen)) < 0) {
-        if (errno != EINTR) fprintf(stderr, "WARN: Error reading ICMP data from %s.\n", p->name);
-        return;
-    } 
-
-    /* check IP header actual len */ 
-    ip       = (struct ip *) buf; 
-    iphdrlen = ip->ip_hl << 2; 
-    icmp     = (struct icmp *) (buf + iphdrlen);
-    
-    if (cc < iphdrlen + ICMP_MINLEN) {
-        fprintf(stderr, "WARN: Received short packet from %s.\n", p->name);
-        return;
-    }
-    
-    if (icmp->icmp_type == ICMP_ECHOREPLY && icmp->icmp_id == ident && icmp->icmp_seq == p->socket) {
-        p->recvdpackets++;
-
-        memcpy(&p->last_ping_received, &tv, sizeof(tv));
-        
-        tvsub(&tv, (struct timeval *) &icmp->icmp_data[0]);
-        delay = tv.tv_sec * 1000 + (tv.tv_usec / 1000);
-        
-        if (isVerbose) printf("INFO: Got ICMP reply from %s in %d ms.\n", p->name, delay);
-        p->down = 0;
-        if (!p->up) {
-            p->up = 1;
-            if (isVerbose) printf("INFO: Host %s is up. Executing UP command.\n", p->name);
-            if (!fork()) {
-                system(p->upcmd);
-                exit(EXIT_SUCCESS);
-            } else {
-                wait(NULL);
-            }
-        }
-    } else {
-        /* TODO: Do anything here? */
-    }
-}
-
-static void
 read_hosts(const char * cfg_file_name)
 {
     int i, n = 0;
     struct Cfg * cfg;
-    
+
     if ((cfg = readcfg(cfg_file_name)) == NULL) {
         fprintf(stderr, "ERROR: Failed to read config.\n");
         exit(EXIT_FAILURE);
     }
-    
+
     if (cfg->nelements) {
         hosts = malloc(sizeof(struct monitor_host *) * cfg->nelements);
         for (i = 0; i < cfg->nelements; i++) {
@@ -332,29 +288,22 @@ read_hosts(const char * cfg_file_name)
                 fprintf(stderr, "ERROR: Too many fields in record %d of cfg file. Got %d.\n", n, cfg->dict[i]->nvalues+1);
                 exit(EXIT_FAILURE);
             }
-            
+
             hosts[n]                = malloc(sizeof(struct monitor_host));
             hosts[n]->name          = strdup(cfg->dict[i]->name);
             hosts[n]->ping_interval = atoi  (cfg->dict[i]->value[0]);
             hosts[n]->max_delay     = atoi  (cfg->dict[i]->value[1]);
             hosts[n]->upcmd         = strdup(cfg->dict[i]->value[2]);
             hosts[n]->downcmd       = strdup(cfg->dict[i]->value[3]);
-            
+
         if (cfg->dict[i]->nvalues == 4) {
-            hosts[n]->down = 0;
-            hosts[n]->up   = 1;
+            hosts[n]->hostup = true;
         } else if (strcmp(cfg->dict[i]->value[4], "up") == 0) {
-            hosts[n]->down = 0;
-            hosts[n]->up   = 1;
+            hosts[n]->hostup = true;
         } else if (strcmp(cfg->dict[i]->value[4], "down") == 0) {
-            hosts[n]->down = 1;
-            hosts[n]->up   = 0;
+            hosts[n]->hostup = false;
         } else if (strcmp(cfg->dict[i]->value[4], "auto") == 0) {
-            hosts[n]->down = 1;
-            hosts[n]->up   = 1;
-        } else if (strcmp(cfg->dict[i]->value[4], "none") == 0) {
-            hosts[n]->down = 0;
-            hosts[n]->up   = 0;
+            /* TODO: Send a ping and set initial state accordingly. */
         } else {
             fprintf(stderr, "ERROR: Illegal value %s in record %d for startup condition.\n", cfg->dict[i]->value[4], n);
             exit(EXIT_FAILURE);
@@ -368,7 +317,7 @@ read_hosts(const char * cfg_file_name)
             gettimeofday(&(hosts[n]->last_ping_received), (struct timezone *)NULL);
 
             n++;
-        } 
+        }
     }
 
     freecfg(cfg);
@@ -384,13 +333,21 @@ gethostaddr(const char * name)
 {
     static int res;
     struct hostent * he;
-    
+
     if ((res = inet_addr(name)) < 0) {
     he = gethostbyname(name);
         if (!he) return -1;
         memcpy(&res, he->h_addr, he->h_length);
-    } 
+    }
     return(res);
+}
+
+static int
+gcd(int x, int y)
+{
+    int remainder = x % y;
+    if (remainder == 0) return y;
+    return gcd(y, remainder);
 }
 
 static void
@@ -404,7 +361,7 @@ init_hosts(void)
         fprintf(stderr, "ERROR: Unknown protocol: icmp.\n");
         exit(EXIT_FAILURE);
     }
-    
+
     while (p) {
         bzero(&p->dest, sizeof(p->dest));
         p->dest.sin_family = AF_INET;
@@ -430,25 +387,46 @@ init_hosts(void)
     }
 }
 
-/*
- * Subtracts two timeval structs.
- * Ensure out >= in.
- * Modifies out = out - in.
- */
-static void
-tvsub(register struct timeval * out, register struct timeval * in)
+int
+main(int argc, char ** argv)
 {
-    if ((out->tv_usec -= in->tv_usec) < 0) {
-        --out->tv_sec;
-        out->tv_usec += 1000000;
-    }
-    out->tv_sec -= in->tv_sec;
-}
+    extern char * optarg;
+    extern int optind;
+    char * cfgfile = NULL;
+    int param;
 
-static int
-gcd(int x, int y)
-{
-    int remainder = x % y;
-    if (remainder == 0) return y;
-    return gcd(y, remainder);
+    while ((param = getopt(argc, argv, "rvf:")) != -1) {
+        switch(param) {
+            case 'v':
+                isVerbose = 1;
+                break;
+            case 'r':
+                keepBanging = 1;
+                break;
+            case 'f':
+                cfgfile=strdup(optarg);
+                break;
+            default:
+                fprintf(stderr,"Usage: icmpmonitor [-v] [-r] [-f cfgfile]\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (!cfgfile) {
+        fprintf(stderr, "ERROR: No config file specified.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    read_hosts(cfgfile);
+
+    init_hosts();
+
+    ident = getpid() & 0xFFFF;
+
+    signal(SIGALRM, pinger);
+    alarm(send_delay);
+
+    get_response();
+
+    exit(EXIT_SUCCESS);
 }
