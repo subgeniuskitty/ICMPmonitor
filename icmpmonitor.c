476ea14deedd4013ab02425e5a1791cbf5e4ba2c
Aaron Taylor <ataylor@subgeniuskitty.com>
2019-09-21 00:20:10 -0700

    First step in consolidating license info into a single file.

--- icmpmonitor.c
+++ icmpmonitor.c
@@ -1,57 +1,11 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Mike Muuss.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * Monitor hosts using ICMP "echo" and notify when down.
+ * 
+ * © 2019 Aaron Taylor <ataylor at subgeniuskitty dot com>
+ * © 1999 Vadim Zaliva <lord@crocodile.org>
+ * © 1989 The Regents of the University of California & Mike Muuss
+ * See LICENSE.txt file for copyright and license details.
  */
-
-/*
- *			ICMPMONITOR.C
- *
- * Using the InterNet Control Message Protocol (ICMP) "ECHO" facility,
- * monitor several hosts, and notify admin if some of them are down.
- *
- * Author -
- *      Vadim Zaliva <lord@crocodile.org>
- *
- * Status -
- *	Public Domain.  Distribution Unlimited.
- */
-
-char copyright[] =
-"@(#) Copyright (c) 1989 The Regents of the University of California.\n"
-"All rights reserved.\n";
-
-char rcsid[] = "$Id: icmpmonitor.c,v 1.8 2004/05/28 01:33:07 lord Exp $";
 
 #include <sys/param.h>
 #include <stdio.h>
